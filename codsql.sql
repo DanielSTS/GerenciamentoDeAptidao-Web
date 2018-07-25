6a3a200b64690f792394bf02cd0f44e8cc241a09
Daniel <daniel.junior.ifpb@gmail.com>
2018-07-25 10:32:57 -0300

    adicionando script BD

---
+++ codsql.sql
@@ -0,0 +1,45 @@
+create database ProjetoBD;
+
+create table ProjetoBD.professor (matricula integer primary key,
+						email varchar(100),
+                        idade integer,
+                        nome varchar(100),
+                        areadeformacao varchar(100),
+                        sexo varchar(100));
+			
+
+create table ProjetoBD.disciplina (codigo integer  primary key,
+						nome varchar(100));
+                        
+create table ProjetoBD.professor_disciplina(codigo_p integer,
+						 codigo_d integer,
+                         foreign key (codigo_p) references ProjetoBD.professor(matricula),
+						 foreign key (codigo_d) references ProjetoBD.disciplina(codigo));
+
+create table ProjetoBD.aula (numero integer primary key,
+						assunto varchar(100),
+                        material varchar(100),
+                        codigo_d integer,
+                        foreign key(codigo_d) references ProjetoBD.disciplina(codigo));
+                        
+create table ProjetoBD.prova (assunto varchar(100),
+						numero integer primary key,
+						matricula_professor integer,
+                        codigo_d integer,
+                        foreign key(codigo_d)references ProjetoBD.disciplina(codigo));
+
+create table ProjetoBD.aluno (matricula integer primary key,
+						curso varchar(100),
+                        nome varchar(100),
+                        codigo_d integer,
+                        foreign key (codigo_d) references ProjetoBD.disciplina(codigo));
+                        
+create table ProjetoBD.prova_aluno (codigo_p integer,
+						codigo_a integer,
+                        nota double precision,
+                        foreign key (codigo_p) references ProjetoBD.prova(numero),
+                        foreign key (codigo_a) references ProjetoBD.aluno(matricula));
+                        
+                        
+                        
+                        
